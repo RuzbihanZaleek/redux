c5e851bebb7cb59538ccdaabb8bc66985f3fd8e6
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2024-03-01 19:32:11 +0530

    L1.6 Creating more slice actions

--- 01_lesson/src/features/counter/Counter.jsx
+++ 01_lesson/src/features/counter/Counter.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from './counterSlice';
+import { decrement, increment, reset, incrementByAmount } from './counterSlice';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+
+  const [addAmount, setAddAmount] = useState(0);
+  const addValue = Number(addAmount) || 0;
+
+  const resetAll = () => {
+    setAddAmount(0);
+    dispatch(reset());
+  };
+
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input type="text" value={addAmount} onChange={(e) => setAddAmount(e.target.value)} />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+        <button onClick={resetAll}>Reset</button>
       </div>
     </section>
   );
